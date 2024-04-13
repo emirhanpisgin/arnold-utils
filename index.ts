397d30696eb23219f6b832c2f3a7b2a9b64e6cf7
kryparnold <emirhanpgt@gmail.com>
2024-04-13 17:37:47 +0300

    implemented auto-role function and welcome message

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-import { Client, Events, IntentsBitField } from "discord.js";
+import { Client, Events, IntentsBitField, type GuildTextBasedChannel } from "discord.js";
+import { config } from "dotenv";
+config();
 
 const client = new Client({
 	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers],
 });
 
 client.on(Events.ClientReady, async (client) => {
-    
+	console.log(client.user.displayName + " is ready!");
 });
+
+client.on(Events.GuildMemberAdd, async (member) => {
+	const userRoleId = "733818614561636382";
+	const welcomeChannel = (await member.guild.channels.fetch("1227706309390893076")) as GuildTextBasedChannel;
+
+	await member.roles.add(userRoleId);
+	await welcomeChannel.send(
+		`<@${member.id}> sunucuya hoşgeldin. Minecraft sunucusuna katılmak için <#1228697461883928697> kanalını okumayı unutma, iyi eğlenceler :)`
+	);
+});
+
+client.login(process.env.TOKEN);
